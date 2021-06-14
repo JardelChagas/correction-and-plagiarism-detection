531e82854ba9f3e94408aa2a1473169159fcb6fc
jardel <jardelchags@gmail.com>
2021-06-14 19:20:17 -0300

    init

---
+++ src/main/kotlin/com/example/correctionandplagiarismdetection/CorrectionAndPlagiarismDetectionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.correctionandplagiarismdetection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CorrectionAndPlagiarismDetectionApplication
+
+fun main(args: Array<String>) {
+    runApplication<CorrectionAndPlagiarismDetectionApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/correctionandplagiarismdetection/CorrectionAndPlagiarismDetectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.correctionandplagiarismdetection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CorrectionAndPlagiarismDetectionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
